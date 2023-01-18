93d9cfb1d1b0aceb6d4ecb28c4c284dec38df945
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2023-01-17 23:47:01 -0800

    Set correct permissions for ssh key after recovery * Error encountered after most recent nuke * SSH key permiisions were too open

--- recovery.sh
+++ recovery.sh
@@ -3,6 +3,7 @@ rm -rf ~/.thunderbird # remove default thunderbird config (should be empty folde
 rclone config reconnect remote: --auto-confirm
 rclone copy remote: ~ -P --fast-list --checkers=32 --transfers=16
 
+chmod 600 ~/.ssh/id_rsa # set the correct permissions for private keys
 git clone git@github.com:colonelwatch/logseq ~/Logseq # only works after ssh key is recovered
 
 chsh -s /bin/fish
