cd13a3c4bb06ecdf4b3669146719e048194e8f72
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2023-03-12 23:54:55 -0700

    Refactor bootstrap.sh toward reducing redundancy

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,40 +1,19 @@
-# SYSTEM CONFIGURATION
+# <SETUP>
 
-sudo cp -f root/pacman.conf /etc/
+# check if pwd is ~/.dotfiles
+if [ ! "$PWD" = "$HOME/.dotfiles" ]; then
+    echo "Please run this script from the ~/.dotfiles directory."
+    exit 1
+fi
+
+# acquire dependencies
+git submodule update --init --recursive
+
+# configure pacman
+sudo cp -f root/etc/pacman.conf /etc/
 sudo pacman -Syuu --noconfirm
 
-sudo pacman -S bolt linux-zen-headers nvidia-dkms udiskie udisks2 --noconfirm # necessary for eGPU setup
-
-# configure bootloader
-sudo cp -f root/boot/loader/entries/arch.conf /boot/loader/entries/
-sudo cp -f root/boot/loader/loader.conf /boot/loader/
-
-# setting frequency to the maximum is needed before calling the undervolt
-sudo pacman -S cpupower --noconfirm
-sudo cp -f root/etc/default/cpupower /etc/default/
-sudo cpupower frequency-set --governor performance
-sudo systemctl enable cpupower.service
-
-sudo cp -f root/etc/systemd/zram-generator.conf /etc/systemd/ # expands swap space to RAM size
-
-sudo pacman -S cronie networkmanager samba jre-openjdk libimobiledevice sof-firmware --noconfirm # system essentials
-sudo systemctl enable cronie.service
-sudo systemctl enable NetworkManager.service
-
-sudo pacman -S bluez bluez-utils --noconfirm
-sudo cp -f root/etc/bluetooth/main.conf /etc/bluetooth/
-sudo systemctl enable bluetooth.service
-
-sudo pacman -S acpi alsa-utils brightnessctl ttf-font-awesome playerctl xclip --noconfirm # used by AwesomeWM exts
-
-sudo cp -f root/etc/X11/xorg.conf.d/* /etc/X11/xorg.conf.d/
-
-
-# PROGRAM INSTALLATION
-
-sudo pacman -S gnome-keyring --noconfirm # deps not covered in proceeding installs
-
-# installing yay manually before using yay to get other AUR programs
+# installing yay manually before using yay
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg --syncdeps --rmdeps --noconfirm
@@ -42,70 +21,83 @@ sudo pacman -U *.pkg.tar.zst --noconfirm
 cd ..
 rm -rf yay-bin
 
-sudo pacman -S rclone --noconfirm # used for restore and automatic backups of home
+# </SETUP>
 
-sudo pacman -S ttf-jetbrains-mono otf-ipafont --noconfirm # fonts
-yay -S ttf-ms-win10-auto --noconfirm --removemake --answerdiff=None --sudoloop # msfonts known to need sudoloop
 
+
+# <ROOT>
+
+yay -S \
+    acpi alsa-utils bluez bluez-utils bolt brightnessctl cpupower cronie \
+    jre-openjdk libimobiledevice linux-zen-headers networkmanager nvidia-dkms \
+    playerctl samba sof-firmware ttf-font-awesome udiskie udisks2 xclip \
+    --noconfirm --removemake --answerdiff=None --sudoloop
+
+# install config files
+sudo cp -rvf --no-preserve=mode,ownership root/boot/loader/* /boot/loader/
+sudo cp -rvf --no-preserve=mode,ownership root/etc/* /etc/
+
+sudo systemctl enable \
+    bluetooth.service cpupower.service cronie.service NetworkManager.service
+
+# </ROOT>
+
+
+
+# <USER>
+
+# install packages
+yay -S \
+    audacity calibre discord firefox fish gimp gnome-keyring google-chrome \
+    gtk-theme-numix-solarized imagemagick inkscape libreoffice-fresh \
+    logseq-desktop-bin lutris macchina-bin man nm-connection-editor otf-ipafont \
+    pcmanfm piavpn-bin qemu-desktop ranger rclone rofi ruby steam teensyduino \
+    thunderbird ttf-jetbrains-mono ttf-ms-win10-auto unzip vim virt-manager \
+    visual-studio-code-bin vlc zip zoom zotero-bin \
+    --noconfirm --removemake --answerdiff=None --sudoloop
+
+# install jekyll through ruby
+gem install jekyll bundler
+
+# install passmark performance test
 yay -S ncurses5-compat-libs --noconfirm --removemake --answerdiff=None
 wget https://www.passmark.com/downloads/pt_linux_x64.zip -O ~/pt_linux_x64.zip
 unzip ~/pt_linux_x64.zip
 sudo cp -f ~/PerformanceTest/pt_linux_x64 /usr/bin/pt # typical calls are: `pt` or `pt -r 1`
 rm -rf ~/pt_linux_x64.zip ~/PerformanceTest
 
-sudo pacman -S vim man rofi pcmanfm vlc fish zip unzip ranger nm-connection-editor --noconfirm # desktop essentials
-yay -S macchina-bin gtk-theme-numix-solarized piavpn-bin --noconfirm --removemake --answerdiff=None
-sudo pacman -S firefox discord thunderbird --noconfirm # web essentials
-yay -S google-chrome slack-desktop zoom logseq-desktop-bin --noconfirm --removemake --answerdiff=None
-sudo pacman -S steam lutris --noconfirm # gaming
-sudo pacman -S ruby --noconfirm # programming
-yay -S visual-studio-code-bin teensyduino --noconfirm --removemake --answerdiff=None
+# download and execute miniconda install script
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3_install.sh
-sudo pacman -S audacity calibre gimp inkscape imagemagick libreoffice-fresh virt-manager qemu-desktop --noconfirm # other tools
-yay -S zotero-bin --noconfirm --removemake --answerdiff=None
-
-sudo systemctl enable piavpn.service
-sudo usermod -a -G uucp kenny
-
-sudo cp -f root/etc/libvirt/libvirtd.conf /etc/libvirt/
-sudo usermod -a -G libvirt kenny
-
-gem install jekyll bundler
 bash ~/miniconda3_install.sh -b # conda will soon be intialized by importing the fish config
-
-sudo pacman -Sc --noconfirm
-yay -Sc --noconfirm
 rm ~/miniconda3_install.sh
 
-sudo cp -f root/usr/share/applications/steam.desktop /usr/share/applications/
-sudo cp -f root/usr/share/applications/audacity.desktop /usr/share/applications/
-sudo cp -f root/usr/share/applications/org.inkscape.Inkscape.desktop /usr/share/applications/
-sudo cp -f root/usr/share/applications/arduino.desktop /usr/share/applications/
+# override .desktops with custom ones
+sudo cp -rvf --no-preserve=mode,ownership root/usr/share/applications/* /usr/share/applications/
 
-
-
-# USER-SPACE CONFIGURATION
-
-crontab crontab.bak
-
-git submodule update --init --recursive
+# install config files
 mkdir -p ~/.config
+ln -s -f $PWD/home/* ~/ # we explicitly need to write out the pwd
+ln -s -f $PWD/config/* ~/.config/
+sudo cp -f root/etc/libvirt/libvirtd.conf /etc/libvirt/ # libvirt is actually a root service!
 
-mkdir -p ~/.config/rclone # not symlink b/c will contain keys
+# deal with rclone config edge case
+unlink ~/.config/rclone # undo symlink b/c it eventually contains keys we don't want to commit...
+mkdir ~/.config/rclone  #  ...so we'll only copy the config files
 cp ~/.dotfiles/config/rclone/rclone.conf ~/.config/rclone/
 # rclone is not authorized yet, so authorize manually in recovery.sh
 
-ln -s -f ~/.dotfiles/home/.bashrc ~/
-ln -s -f ~/.dotfiles/home/.gtkrc-2.0 ~/
-ln -s -f ~/.dotfiles/home/.gitconfig ~/
-ln -s -f ~/.dotfiles/home/.Xresources ~/
-ln -s -f ~/.dotfiles/config/awesome ~/.config/
-ln -s -f ~/.dotfiles/config/alacritty ~/.config/
-ln -s -f ~/.dotfiles/config/chrome-flags.conf ~/.config/
-ln -s -f ~/.dotfiles/config/fish ~/.config/
-ln -s -f ~/.dotfiles/config/gtk-3.0 ~/.config/
-ln -s -f ~/.dotfiles/config/macchina ~/.config/
-ln -s -f ~/.dotfiles/config/lutris ~/.config/
-ln -s -f ~/.dotfiles/config/rofi ~/.config/
-
+# other config
+sudo systemctl enable piavpn.service
+sudo usermod -a -G uucp,libvirt kenny # needed for arduino and virt-manager
 xdg-mime default pcmanfm.desktop inode/directory # by default, vscode seems to be the default file manager
+crontab crontab.bak
+
+# </USER>
+
+
+
+# <CLEANUP>
+
+yay -Sc --noconfirm
+
+# </CLEANUP>
