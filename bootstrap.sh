c42b9495d7754cbe775b2feb6ef31a2f435b1361
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2024-02-13 17:32:42 -0800

    Switch back to proprietary blob to fix dropping GPUs

--- bootstrap.sh
+++ bootstrap.sh
@@ -29,7 +29,7 @@ rm -rf yay-bin
 
 yay -S \
     acpi alsa-utils bluez bluez-utils bolt brightnessctl cpupower cronie dnsmasq gksu \
-    jre-openjdk libimobiledevice linux-zen-headers networkmanager nvidia-open-dkms \
+    jre-openjdk libimobiledevice linux-zen-headers networkmanager nvidia-dkms \
     playerctl samba sof-firmware ttf-font-awesome udiskie udisks2 xclip \
     --noconfirm --removemake --answerdiff=None --sudoloop
 
