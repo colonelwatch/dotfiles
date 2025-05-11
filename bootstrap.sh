810f6ecada80b37674aef81dc982b10e8620a356
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2025-05-11 16:41:10 -0700

    Add jq and drop Calibre

--- bootstrap.sh
+++ bootstrap.sh
@@ -50,8 +50,8 @@ sudo systemctl enable \
 
 # install packages
 MAKEFLAGS="-j$(nproc)" yay -S \
-    audacity calibre discord firefox fish freecad fuse2 ghostscript gimp git-lfs gnome-keyring google-chrome \
-    gparted imagemagick inkscape kicad kicad-library kicad-library-3d libreoffice-fresh \
+    audacity discord firefox fish freecad fuse2 ghostscript gimp git-lfs gnome-keyring google-chrome \
+    gparted imagemagick inkscape jq kicad kicad-library kicad-library-3d libreoffice-fresh \
     logseq-desktop-bin lutris macchina man nm-connection-editor otf-ipafont parallel \
     pcmanfm perl-image-exiftool prusa-slicer qemu-desktop ranger rclone rsync rofi rpcs3-bin ruby steam \
     teensyduino thunderbird ttf-ia-writer ttf-jetbrains-mono ttf-ms-win10-cdn turbostat unzip vim virt-manager \
