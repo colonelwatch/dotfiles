7041d374b836797cb57d1e8e68f2d07fb05349eb
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2025-06-17 00:17:46 -0700

    Use a dedicated vim-like config when calling neovim via the vim alias

--- config/fish/functions/vim.fish
+++ config/fish/functions/vim.fish
@@ -1,4 +1,4 @@
-function vim --wraps=nvim --description 'alias vim nvim'
-  nvim $argv
+function vim --wraps=nvim --wraps='NVIM_APPNAME=nvim-vim nvim' --description 'alias vim NVIM_APPNAME=nvim-vim nvim'
+  NVIM_APPNAME=nvim-vim nvim $argv
         
 end
