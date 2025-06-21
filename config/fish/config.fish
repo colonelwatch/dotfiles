8ccd2f2e932872f73e64624201bde023eb64ef98
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2025-06-21 09:01:26 -0700

    Stop tracking fish_variables and reimplement its changes in config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,11 +4,17 @@ end
 
 set fish_greeting
 
+set -gx EDITOR nvim
+set -gx GEM_HOME "$(gem env user_gemhome)"
+
+fish_add_path -g "$GEM_HOME/bin"
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 eval /home/kenny/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
-if status is-interactive # macchina is called last because it seems to prevent a glitch
+# Unlike other interactive cmds, run macchina last to stop a graphical glitch
+if status is-interactive
     macchina
 end
