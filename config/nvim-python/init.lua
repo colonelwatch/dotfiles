3fbdcb0cf3afd7453aca47feca54e6e6d325bcb4
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2025-06-22 08:22:35 -0700

    Delete an additional help comment

--- config/nvim-python/init.lua
+++ config/nvim-python/init.lua
@@ -535,11 +535,7 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  { -- color scheme
     'Tsuzat/NeoSolarized.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- Make sure to load this before all the other start plugins.
