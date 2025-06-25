5830d2e9893e6404a053d4820a6b3852dbeb16b3
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2025-06-24 21:04:09 -0700

    On save, use ruff formatter and isort instead of the defaults

--- config/nvim-python/init.lua
+++ config/nvim-python/init.lua
@@ -474,7 +474,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- python = { "isort", "black" },
+        python = { 'ruff_format', 'ruff_organize_imports' },
       },
     },
   },
