96d81e67570f06b5302be6ef0227cfd8d113d2dc
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2025-06-24 21:35:45 -0700

    Set my familiar rulers in nvim-python

--- config/nvim-python/init.lua
+++ config/nvim-python/init.lua
@@ -65,6 +65,9 @@ vim.o.confirm = true
 -- Set the window border (across all plugins)
 vim.o.winborder = 'single'
 
+-- Set the rulers
+vim.o.colorcolumn = '81,89'
+
 -- [[ Basic Keymaps ]]
 
 -- Clear highlights on search when pressing <Esc> in normal mode
