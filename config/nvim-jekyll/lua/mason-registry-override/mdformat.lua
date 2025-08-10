3ad6ae98e072a518034a05675c759f7c8e4ac69f
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2025-08-10 11:52:14 -0700

    Replace mdformat-frontmatter with mdformat-myst for math support

--- config/nvim-jekyll/lua/mason-registry-override/mdformat.lua
+++ config/nvim-jekyll/lua/mason-registry-override/mdformat.lua
@@ -7,7 +7,7 @@ return {
     categories = { "Formatter" },
     source = {
         id = "pkg:pypi/mdformat@0.7.22",
-        extra_packages = { "mdformat-frontmatter==2.0.8" },
+        extra_packages = { "mdformat-myst==0.2.1" },
     },
     bin = {
         ["mdformat"] = "pypi:mdformat",
