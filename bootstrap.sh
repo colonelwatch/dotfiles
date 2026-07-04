cc43c39f21ed68d5deb9fd296dc9098366da7bba
Kenny Peng <53552802+colonelwatch@users.noreply.github.com>
2026-07-03 22:59:33 -0700

    Add back `gparted`
    This time, it doesn't need `gksu` because the polkit integration is
    working.

--- bootstrap.sh
+++ bootstrap.sh
@@ -52,12 +52,13 @@ sudo systemctl enable                                   \
 # install packages
 yay -S --noconfirm --answerdiff=None --sudoloop                             \
     audacity antigravity antigravity-cli antigravity-ide bc bluetui discord \
-    firefox fish freecad fd github-cli ghostscript gimp git-lfs imagemagick \
-    inkscape jq kicad kicad-library kicad-library-3d libreoffice-fresh      \
-    ltspice lutris macchina man neovim parallel perl-image-exiftool         \
-    picom-git polkit-gnome pre-commit prismlauncher prusa-slicer pyenv      \
-    rpi-imager ripgrep rclone rsync rofi ruby steam thunderbird tree        \
-    tree-sitter-cli unzip virt-manager vlc xclip yazi zip zopfli
+    firefox fish freecad fd github-cli ghostscript gimp git-lfs gparted     \
+    imagemagick inkscape jq kicad kicad-library kicad-library-3d            \
+    libreoffice-fresh ltspice lutris macchina man neovim parallel           \
+    perl-image-exiftool picom-git polkit-gnome pre-commit prismlauncher     \
+    prusa-slicer pyenv rpi-imager ripgrep rclone rsync rofi ruby steam      \
+    thunderbird tree tree-sitter-cli unzip virt-manager vlc xclip yazi zip  \
+    zopfli
 
 # install awesome-luajit-git with docs explicitly disabled (breaks with Lua 5.5)
 _BUILD_DOCS=0 yay -S --noconfirm --answerdiff=None awesome-luajit-git
